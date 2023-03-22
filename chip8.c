3f9414f9d50f5f8cbfa5c94885bb0c1c1e138678
Dex-Max <randall.eagan.kim@gmail.com>
2023-03-22 13:45:28 -0400

    Implement opcodes for logic

--- chip8.c
+++ chip8.c
@@ -1,15 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "display.h"
 #include "chip8.h"
 
 void init_chip8(Chip8 *chip8) {
+    uint8_t font[] = {
+        0xf0, 0x90, 0x90, 0x90, 0xf0,
+        0x20, 0x60, 0x20, 0x20, 0x70,
+        0xf0, 0x10, 0xf0, 0x80, 0xf0,
+        0xf0, 0x10, 0xf0, 0x10, 0xf0,
+        0x90, 0x90, 0xf0, 0x10, 0x10,
+        0xf0, 0x80, 0xf0, 0x10, 0xf0,
+        0xf0, 0x80, 0xf0, 0x90, 0xf0,
+        0xf0, 0x10, 0x20, 0x40, 0x40,
+        0xf0, 0x90, 0xf0, 0x90, 0xf0,
+        0xf0, 0x90, 0xf0, 0x10, 0xf0,
+        0xf0, 0x90, 0xf0, 0x90, 0x90,
+        0xe0, 0x90, 0xe0, 0x90, 0xe0,
+        0xf0, 0x80, 0x80, 0x80, 0xf0,
+        0xe0, 0x90, 0x90, 0x90, 0xe0,
+        0xf0, 0x80, 0xf0, 0x80, 0xf0,
+        0xf0, 0x80, 0xf0, 0x80, 0x80
+    };
+    
+    memcpy(chip8->mem, font, sizeof(font));
     memset(chip8->stack, 0, 16 * sizeof(uint16_t));
     chip8->sp = 0;
     memset(chip8->mem, 0, 4096);
     memset(chip8->reg, 0, 16);
     chip8->I_reg = 0;
     chip8->pc = 0x200;
+    memset(chip8->display, 0, 64 * 32);
 }
 
 void load_program(Chip8 *chip8, char filename[]) {
@@ -34,22 +56,122 @@ void load_program(Chip8 *chip8, char filename[]) {
 void execute_opcode(Chip8 *chip8, Display *display) {
     switch (chip8->op >> 12) {
         case 0:
-            if (chip8->op == 0x00e0) {
-                memset(chip8->display, 0, 64 * 32);
+            switch (chip8->op & 0xff) {
+                case 0xe0:
+                    memset(chip8->display, 0, 64 * 32);
+                    chip8->pc += 2;
+                    break;
+                case 0xee:
+                    chip8->sp -= 1;
+                    chip8->pc = chip8->stack[chip8->sp] + 2;
+                    break;
             }
 
             break;
         case 1:
             chip8->pc = chip8->op & 0xfff; 
             break;
+        case 2:
+            chip8->stack[chip8->sp] = chip8->pc;
+            chip8->sp += 1;
+            chip8->pc = chip8->op & 0xfff;
+            break;
+        case 3:
+            if (chip8->reg[(chip8->op >> 8) & 0xf] == (chip8->op & 0xff)) chip8->pc += 2;
+            chip8->pc += 2;
+            break;
+        case 4:
+            if (chip8->reg[(chip8->op >> 8) & 0xf] != (chip8->op & 0xff)) chip8->pc += 2;
+            chip8->pc += 2;
+            break;
+        case 5:
+            if (chip8->reg[(chip8->op >> 8) & 0xf] == chip8->reg[(chip8->op >> 4) & 0xf]) chip8->pc += 2;
+            chip8->pc += 2;
+            break;
         case 6:
             chip8->reg[(chip8->op >> 8) & 0xf] = chip8->op & 0xff;
+            chip8->pc += 2;
             break;
         case 7:
             chip8->reg[(chip8->op >> 8) & 0xf] += chip8->op & 0xff;
+            chip8->pc += 2;
+            break;
+        case 8:
+            int reg_x = (chip8->op >> 8) & 0xf;
+            int reg_y = (chip8->op >> 4) & 0xf;
+
+            switch (chip8->op & 0xf) {
+                case 0:
+                    chip8->reg[reg_x] = chip8->reg[reg_y];
+                    break;
+                case 1:
+                    chip8->reg[reg_x] = chip8->reg[reg_x] | chip8->reg[reg_y];
+                    break;
+                case 2:
+                    chip8->reg[reg_x] = chip8->reg[reg_x] & chip8->reg[reg_y];
+                    break;
+                case 3:
+                    chip8->reg[reg_x] = chip8->reg[reg_x] ^ chip8->reg[reg_y];
+                    break;
+                case 4:
+                    uint8_t add = chip8->reg[reg_x] + chip8->reg[reg_y];
+
+                    if (add < chip8->reg[reg_x]) {
+                        chip8->reg[0xf] = 1;
+                    } else {
+                        chip8->reg[0xf] = 0;
+                    }
+
+                    chip8->reg[reg_x] = add;
+                    break;
+                case 5:
+                    uint8_t sub = chip8->reg[reg_x] - chip8->reg[reg_y];
+
+                    if (chip8->reg[reg_y] > chip8->reg[reg_x]) {
+                        chip8->reg[0xf] = 1;
+                    } else {
+                        chip8->reg[0xf] = 0;
+                    }
+
+                    chip8->reg[reg_x] = sub;
+                    break;
+                case 6:
+                    chip8->reg[0xf] = chip8->reg[reg_x] & 1;
+                    chip8->reg[reg_x] >>= 1;
+                    break;
+                case 7:
+                    uint8_t subn = chip8->reg[reg_y] - chip8->reg[reg_x];
+
+                    if (chip8->reg[reg_y] > chip8->reg[reg_x]) {
+                        chip8->reg[0xf] = 1;
+                    } else {
+                        chip8->reg[0xf] = 0;
+                    }
+
+                    chip8->reg[reg_x] = subn;
+                    break;
+                case 0xE:
+                    chip8->reg[0xf] = chip8->reg[reg_x] >> 7;
+                    chip8->reg[reg_x] <<= 1;
+                    break;
+            }
+
+            chip8->pc += 2;
+            break;
+        case 9:
+            if (chip8->reg[(chip8->op >> 8) & 0xf] != chip8->reg[(chip8->op >> 4) & 0xf]) chip8->pc += 2;
+            chip8->pc += 2;
             break;
         case 0xA:
             chip8->I_reg = chip8->op & 0xfff; 
+            chip8->pc += 2;
+            break;
+        case 0xB:
+            chip8->pc = (chip8->op & 0xfff) + chip8->reg[0];
+            break;
+        case 0xC:
+            chip8->reg[(chip8->op >> 4) & 0xf] = (rand() % 0xff) & (chip8->op & 0xff);
+            chip8->pc += 2;
             break;
         case 0xD:
             int x = chip8->reg[(chip8->op >> 8) & 0xf] % 64;
@@ -72,20 +194,43 @@ void execute_opcode(Chip8 *chip8, Display *display) {
                         }
 
                         chip8->display[(y + i) * 64 + (x + p)] ^= (sprite >> (7 - p)) & 1;
-                        
                     }
                 }
             }
 
             update_display(chip8, display);
+            chip8->pc += 2;
 
             break;
+        default:
+            printf("Invalid opcode");
+            chip8->pc += 2;
     }
+}
+
+void debug(Chip8 *chip8) {
+    int i;
+    printf("**********************************\n");
+    printf("Program counter: %x\n", chip8->pc);
+    printf("Current op: %x\n", chip8->op);
+    printf("Registers:\n");
+    for (i = 0; i < 16; i++) {
+        printf("V%x: %x | ", i, chip8->reg[i]);
+    }
+    printf("I reg: %x\n", chip8->I_reg);
+    printf("Stack pointer: %x\n", chip8->sp);
+    printf("Stack: ");
+    for (i = 0; i < 16; i++) {
+        printf("[%d]: %x  |  ", i, chip8->stack[i]);
+    }
+    printf("\n**********************************\n");
 }
 
 int main(int argc, char *argv[]) {
     Chip8 chip8;
     Display display;
+    
+    srand(time(NULL));
 
     init_chip8(&chip8);
     init_display(&display);
@@ -93,12 +238,11 @@ int main(int argc, char *argv[]) {
     load_program(&chip8, argv[1]);
 
     while (chip8.pc < 4096) {
+        getchar();
+        debug(&chip8);
         chip8.op = (chip8.mem[chip8.pc] << 8) | chip8.mem[chip8.pc + 1];
         execute_opcode(&chip8, &display);
-        chip8.pc += 2; 
     }
-
-    while(1) {}
 
     cleanup();
 
